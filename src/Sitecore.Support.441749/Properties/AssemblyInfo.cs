ec5b5cc307049014c4a258e3cbf7ca6388159974
ido <ido@WS-IDO1UA1>
2017-03-07 22:03:42 +0200

    First commit

---
+++ src/Sitecore.Support.441749/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.441749")]
+[assembly: AssemblyProduct("Sitecore.Support.441749")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Media advanced upload does not save the Alt field")]
+
